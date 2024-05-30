8956b62d526e03967e410e767c23ec2dec768d9c
Marlon <eletromarlon@gmail.com>
2024-05-30 14:10:07 -0300

    Modify client to receive address by multicast

--- server-client_JSON/client_grpc_JSON.py
+++ server-client_JSON/client_grpc_JSON.py
@@ -1,3 +1,66 @@
+import socket
+import struct
+import grpc
+import grpc_image2_pb2
+import grpc_image2_pb2_grpc
+import time
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+def sca_shipper(
+    id_sender: str = 'Who?',
+    server_ip: str = 'localhost',
+    server_port: str = '50051',
+    type: int = 0,
+    name: str = 'vazio',
+    turma_id: str = 'vazio',
+    disciplina_id: str = 'vazio',
+    aluno_id: str = 'vazio',
+    time: float = time.time(),
+    img_data: str = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDATx\x9cc``\x00\x00\x00\x02\x00\x01\xe2!\xbc\x82\x00\x00\x00\x00IEND\xaeB`\x82'
+):
+    '''
+    Função ... escrever a documentação
+    '''
+    with grpc.insecure_channel(server_ip +':'+ server_port) as channel:
+        stub = grpc_image2_pb2_grpc.EnvioDeMensagensStub(channel)
+        request = grpc_image2_pb2.ClienteParaServidor(
+            type=type,
+            id_turma=turma_id,
+            id_disciplina=disciplina_id,
+            id_aluno=aluno_id,
+            time=time,
+            image= img_data,
+            name=name
+        )
+        response = stub.EnviarMensagem(request)
+        print("Resposta recebida:")
+        print(f"type: {response.type}")
+        print(f"id_turma: {response.id_turma}")
+        print(f"id_disciplina: {response.id_disciplina}")
+        print(f"id_aluno: {response.id_aluno}")
+        print(f"time: {response.time}")
+        print(f"name: {response.name}")
+        print(f"num_faltas: {response.num_faltas}")
+        
+        return response
+        # Use response.image e response.repositorio conforme necessário
+
+def discover_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', MCAST_PORT))
+
+    mreq = struct.pack('4sl', socket.inet_aton(MCAST_GRP),socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    
+    print("Procurando um servidor!\n...")
+    while True:
+        data, addr = sock.recvfrom(1024)
+        print(f"Mensagem recebida: {data} de {addr}")
+        return addr[0]
+    
 '''
 import json
 import grpc_image2_pb2 as pb2
@@ -45,79 +108,3 @@ def sca_shipper(
 
         return resposta_data
 '''
-import grpc
-import grpc_image2_pb2
-import grpc_image2_pb2_grpc
-import time
-
-def run():
-    with grpc.insecure_channel('192.168.0.13:50051') as channel:
-        stub = grpc_image2_pb2_grpc.EnvioDeMensagensStub(channel)
-        request = grpc_image2_pb2.ClienteParaServidor(
-            type=1,
-            id_turma="turma1",
-            id_disciplina="disciplina1",
-            id_aluno="aluno1",
-            time=int(time.time()),
-            image=b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x06\x00\x00\x00\x1f\x15\xc4\x89\x00\x00\x00\nIDATx\x9cc``\x00\x00\x00\x02\x00\x01\xe2!\xbc\x82\x00\x00\x00\x00IEND\xaeB`\x82',
-            name="John Doe"
-        )
-        response = stub.EnviarMensagem(request)
-        print("Resposta recebida:")
-        print(f"type: {response.type}")
-        print(f"id_turma: {response.id_turma}")
-        print(f"id_disciplina: {response.id_disciplina}")
-        print(f"id_aluno: {response.id_aluno}")
-        print(f"time: {response.time}")
-        print(f"name: {response.name}")
-        print(f"num_faltas: {response.num_faltas}")
-        # Use response.image e response.repositorio conforme necessário
-
-if __name__ == '__main__':
-    run()
-
-
-'''
-
-
-def enviar_mensagem(canal, chave, valor):
-  # Cria mensagem proto
-  mensagem_proto = pb2.Mensagem(chave=chave, valor=valor)
-
-  # Envia mensagem proto para o servidor
-  stub = pb2_grpc.ServicoGprcStub(canal)
-  resposta_proto = stub.EnviarMensagem(mensagem_proto)
-
-  # Converte mensagem proto para JSON
-  resposta_json = {
-      "chave": resposta_proto.chave,
-      "valor": resposta_proto.valor
-  }
-
-  print(f"Resposta do servidor: {resposta_json}")
-
-def receber_mensagens(canal):
-  # Cria stub do servidor
-  stub = pb2_grpc.ServicoGprcStub(canal)
-
-  # Recebe stream de mensagens proto
-  for resposta_proto in stub.ReceberMensagem():
-    # Converte mensagem proto para JSON
-    resposta_json = {
-        "chave": resposta_proto.chave,
-        "valor": resposta_proto.valor
-    }
-
-    print(f"Mensagem recebida: {resposta_json}")
-
-channel = grpc.insecure_channel('localhost:50051')
-stub = pb2_grpc.ServicoGprcStub(channel)
-
-json_file = {
-  "nome":"marlon",
-  "idade":'33'
-}
-
-enviar_mensagem(channel, json_file["nome"], json_file["idade"])
-'''
-# ... (criação do canal gRPC e execução das funções)
