5ac6ecdae107d08105447f5b15a671bfe3fd3333
eletromarlon <eletromarlon@gmail.com>
2024-05-30 15:00:50 -0300

    Create server and client to receive address from server

---
+++ server-client_JSON/sca_discover_client.py
@@ -0,0 +1,27 @@
+import socket
+import struct
+import time
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+CLIENT_MSG = b'Cliente buscando servidor'
+
+def run_client():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
+    # Enviar requisição multicast
+    print("Enviando requisição multicast...")
+    sock.sendto(CLIENT_MSG, (MCAST_GRP, MCAST_PORT))
+
+    # Aguardar resposta
+    print("Aguardando resposta do servidor...")
+    sock.settimeout(5)
+    try:
+        data, addr = sock.recvfrom(1024)
+        print(f"Resposta recebida: {data} de {addr}")
+    except socket.timeout:
+        print("Nenhuma resposta recebida dentro do tempo limite.")
+
+#if __name__ == '__main__':
+#    run_client()
