891d83f714a75f873a4731b286e2fc79239a19d8
eletromarlon <eletromarlon@gmail.com>
2024-06-01 23:06:20 -0300

    Try/except on time informarmation

--- server-client_v2/sca_main.py
+++ server-client_v2/sca_main.py
@@ -8,18 +8,26 @@ from cam_auto_take import take_photo
 server_ip = None
 SCA_LOG = []
 
+def time_convert(tempo):
+    try:
+        tempo = time.gmtime(float(tempo))
+    except:
+        print("Erro: Dados de tempo inconsistente ou quebrados durante o envio")
+        return time.gmtime(float(1.1))
+    return tempo
+
+
+# Captura o endereço do servidor, só será usado durante o início do sistema. Enquanto não se encontre o endereço o laço não terminará
 while server_ip == None:
     server_ip = run_client()
 
+# Executa o sistema infinitamente
 while KeyboardInterrupt:
-    imagem = take_photo()
 
-    #print((bytes(imagem))[:10])
+    # Captura da imagem em formado ndarray
+    imagem = take_photo()
     
-    '''img_64 = base64.b64encode(imagem)  #.decode('utf-8')'''
-    
-    '''print(img_64[:100])'''
-    
+    # Enviando dados ao servidor. Por enquanto apenas passando esses parâmetros
     saida  = client.sca_shipper(
         server_ip=server_ip[0],
         img_data=bytes(imagem)
@@ -28,10 +36,11 @@ while KeyboardInterrupt:
     #print(f'Erro\nDados não enviados ou Rosto não reconhecido\nMensagem de erro\n{saida}')
     
     try:
-        tempo = time.gmtime(float(saida.time))
+        tempo = time_convert(saida.time)
         SCA_LOG.append([saida.name, tempo])
         print(f'Dia/Mes/Ano {tempo[2]}/{tempo[1]}/{tempo[0]}')
         print(f"Rosto encontrado {saida.name}")
+        # Isso pode ser excluído. Apenas para analisar as saídas.
         time.sleep(2)
     except:
-        ('Sem dados de tempo')
+        ('Erro na finalização dos dados.')
