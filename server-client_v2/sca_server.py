9b06b509aa9518592170c7714397d12aa4b98471
eletromarlon <eletromarlon@gmail.com>
2024-06-01 21:54:28 -0300

    Update sca_server with comments in code

--- server-client_v2/sca_server.py
+++ server-client_v2/sca_server.py
@@ -1,19 +1,23 @@
 from concurrent import futures
-from deepface_rcgn import stream_compare
+
+import numpy as np
+from sca_recognizer import stream_compare, train
 from sca_discover_server import run_server
 
-import json, cv2, base64, grpc, time, socket, struct, threading
+import json, cv2, base64, grpc, time, socket, struct, threading, os
 import grpc_image2_pb2
 import grpc_image2_pb2_grpc
 
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 5007
 SERVER_PORT = 50051
-MESSAGE = ('Servidor gRPC disponível na porta 50051').encode('utf-8')
+MESSAGE = b'Servidor gRPC disponivel na porta 50051'
 
 class EnvioDeMensagensServicer(grpc_image2_pb2_grpc.EnvioDeMensagensServicer):
     def EnviarMensagem(self, request, context):
         '''
+        Esta função tem a tarefa de receber mensagens dos clintes e as devolver
+
         A mensagem do clinte inclui os seguintes dados:
             type int32;
             id_turma string;
@@ -24,8 +28,24 @@ class EnvioDeMensagensServicer(grpc_image2_pb2_grpc.EnvioDeMensagensServicer):
             name string;
         '''
 
-        print(f'Dados recebidos:\n{request.type}\n{request.id_turma}\n{request.id_disciplina}\n{request.id_aluno}\n{float(request.time)}\n{request.name}')
+        # Decodificando a mensagem e transformando em nparray - os valores em reshape devem ser alterados de acordo com a camera
+        # Quando utilizado o método opencv, esses valores devem ser cuidadosamente alterados
+        image = np.frombuffer(request.image, dtype=np.uint8).reshape(720,1080,4)
 
+        # Salvando a imagem recebida. Essa linha poderá ser apagada caso se possa utilizar o ndarray na lib deepface
+        cv2.imwrite('image.jpg', image)
+       
+       # Realizando o reconhecimento a partir da imagem salva na linha anterior
+        result = stream_compare('/home/avell/Documentos/projetos/Sistema-de-Chamada-Automatica/image.jpg', '/home/avell/Documentos/projetos/Sistema-de-Chamada-Automatica/marlon', 3)
+
+        #print(f'Valor retornado pela função de reconhecimento facial\n{result}')
+
+        try:
+            name = os.path.basename(result[0])
+        except:
+            name = result[0]
+
+        # Preenchendo os valores da resposta do servidor para o cliente
         response = grpc_image2_pb2.ServidorParaCliente()
         response.type = request.type
         response.id_turma = request.id_turma
@@ -33,21 +53,14 @@ class EnvioDeMensagensServicer(grpc_image2_pb2_grpc.EnvioDeMensagensServicer):
         response.id_aluno = request.id_aluno
         response.time = request.time
         response.image = request.image
-        response.name = request.name
+        response.name = name
         response.num_faltas = 0
         response.repositorio = b""
         
         return response
 
-def multicast_announcement():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-    while True:
-        sock.sendto(MESSAGE, (MCAST_GRP, MCAST_PORT))
-        print(f"Anunciando: {MESSAGE}")
-        time.sleep(1)  # Anuncia a cada 5 segundos
-
 def serve():
+    # Padrão em uso de servidores gRPC apenas com aumento de tamanho máximo de resposta
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
 					   options=[('grpc.max_send_message_length', 50 * 1024 * 1024),
 				 				('grpc.max_receive_message_length', 50 * 1024 * 1024)
@@ -57,6 +70,8 @@ def serve():
     server.add_insecure_port('[::]:50051')
     server.start()
     print("Servidor iniciado na porta 50051")
+
+    # Inicia a thread de anúncio multicast
     threading.Thread(run_server(), daemon=True).start()
     try:
         while True:
@@ -64,8 +79,6 @@ def serve():
     except KeyboardInterrupt:
         server.stop(0)
         exit()
-
-    # Inicia a thread de anúncio multicast
 
 if __name__ == '__main__':
     serve()
