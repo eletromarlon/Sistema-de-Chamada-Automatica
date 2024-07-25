e63527a7deab22afbbf9346697acc4eb756dbb43
Marlon Duarte <60488949+eletromarlon@users.noreply.github.com>
2024-07-25 14:35:03 -0300

    Delete old_files directory
    Não havia mais necessidade desse diretório constar aqui no repositorio

--- old_files/README.md
+++
@@ -1,1 +0,0 @@
-# Sistema-de-Chamada-Automatica
--- old_files/raspCamPy.py
+++
@@ -1,7 +0,0 @@
-import os #lib para executar coando no sistema
-
-tempo = "20000" # tempo de execução da camera
-largura = "1000" #width
-altura = "1000" #height
-
-os.system("libcamera-vid -t " + tempo + " --width " + largura + " --height " + altura) #Comandos para rodar a camera
