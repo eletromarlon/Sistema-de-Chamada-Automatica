4cbb1e6c7d8b55f0b2d11c027579e780a813bee0
eletromarlon <eletromarlon@gmail.com>
2024-07-26 14:21:54 -0300

    Adding ssid collect to show on display 1602

--- server_client_v2/network_show.py
+++ server_client_v2/network_show.py
@@ -1,11 +1,12 @@
 import os, subprocess
 from display_1602a import display_lcd
 
-def mostrar_ip(comando):
-    resultado = subprocess.run(comando, shell=True, capture_output=True, text=True)
-    print(resultado.stdout)
-    
-    display_lcd(resultado.stdout, time=30)
+def mostrar_ip(comando1: None, comando2: None):
+    ip = subprocess.run(comando1, shell=True, capture_output=True, text=True)
+    print(ip.stdout)
+    ssid = subprocess.run(comando2, shell=True, capture_output=True, text=True)
+    print(f'{ssid.stdout}@{ip.stdout}')
+    display_lcd(ip.stdout + '@' + ssid.stdout, time=30)
     
 if __name__ == '__main__':
-    mostrar_ip('hostname -I')
+    mostrar_ip('hostname -I', 'iwgetid -r')
