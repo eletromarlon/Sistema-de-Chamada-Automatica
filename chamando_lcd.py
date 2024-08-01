de908baec355436265164426bb16357e7ccb885c
eletromarlon <eletromarlon@gmail.com>
2024-07-31 23:52:30 -0300

    Test

--- chamando_lcd.py
+++ chamando_lcd.py
@@ -2,11 +2,11 @@ from teste_lcd import LCDTask
 
 display = LCDTask()
 
-display.start_display(word="Marlon", time=10)
+display.start_display(word="Marlon")
 print("Imprimindo outras coisas")
 print("Marlon Duarte")
 print("Ivo Duarte")
-display.start_display(word="finalizando", time=10)
+display.start_display(word="finalizando")
 print("Imprimindo outras coisas")
 print("Marlon Duarte")
 print("Ivo Duarte")
