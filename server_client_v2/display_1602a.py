ae2477c01327dc449f5da0350727441a7a75a2e0
eletromarlon <eletromarlon@gmail.com>
2024-08-27 00:46:37 -0300

    Voltando o estado do display

--- server_client_v2/display_1602a.py
+++ server_client_v2/display_1602a.py
@@ -1,87 +1,116 @@
-# lcd_threaded.py
-
-import threading
-import RPi.GPIO as GPIO  # type: ignore
+import RPi.GPIO as GPIO
 from time import sleep
-import lcd1602gpio  # type: ignore
+import lcd1602gpio
 
-class LCDTask:
-    def __init__(self):
-        self._thread = None
-        self._running = threading.Event()
-        self._running.clear()
-        self.word = ""
-        self.row = 0
-        self.time = 2
+'''
+pin = 24
+iterations = 10
+interval = .25
 
-    def _setup_lcd(self):
-        """Configura o LCD para uso."""
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pin, GPIO.OUT)
+
+for x in range(1, iterations+1):
+    print(f"Loop {x}: LED on")
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(interval)
+    
+    print(f"Loop {x}: LED off")
+    GPIO.output(pin, GPIO.LOW)
+    sleep(interval)
+    
+
+'''
+def display_lcd(
+    word: str = '--------------------------------',
+    row: int = 0 | 1,
+    time: int = 2
+):
+    '''
+    Exibir mensagens em um display de 16 segmentos com 2 linhas
+    Para quebrar linha use o caracter '@'
+    Se mais de 32 caracteres forem repassados, a partir do 33º não será exibido
+    
+    Args:
+        word:(string com tamanho maximo de 16 caracteres) Recebe uma string que será exibida no display
+        row: (0 | 1) indica a linha em que a mensagem será exibida
+    
+    Exemplo: display_lcd(word="Marlon @Duarte") --> Marlon
+                                                    Duarte
+                                                    
+    '''
+    if (word.find('@') != -1):
         # Disable GPIO warnings
         GPIO.setwarnings(False)
         # Set GPIO pin mode. RPi pins described in this example use BCM.
         GPIO.setmode(GPIO.BCM)
+
         # create an instance of LCD1602GPIO_4BIT for 4-bit mode.
         # the LCD module must be already powered on here.
         # the instance initializes the LCD module immediately during init.
-        return lcd1602gpio.LCD1602GPIO_4BIT(
-            rs=8,
-            e=7,
-            db7=25,
-            db6=24,
-            db5=23,
-            db4=18
-        )
-
-    def _back_light(self, button: bool):
-        """Controla a luz de fundo do LCD."""
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(12, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.output(12, GPIO.HIGH if button else GPIO.OUT)
-
-    def _display_task(self):
-        """Executa a tarefa de exibição no LCD."""
-        self._back_light(True)
-        lcd = self._setup_lcd()
-
-        try:
-            find_arroba = self.word.find('@')
-        except Exception:
-            find_arroba = -1
-
-        if find_arroba != -1:
-            lcd.write_line(self.word[:find_arroba], 0)
-            lcd.write_line(self.word[find_arroba + 1:], 1)
-        elif len(self.word) > 16:
-            lcd.write_line(self.word[:16], 0)
-            lcd.write_line(self.word[16:], 1)
-        else:
-            lcd.write_line(self.word, self.row)
-
-        sleep(self.time)
+        lcd = lcd1602gpio.LCD1602GPIO_4BIT(
+                rs=8,
+                e=7,
+                db7=18,
+                db6=23,
+                db5=24,
+                db4=25)
+        # write texts to Line 0 of the LCD.
+        lcd.write_line(word[:word.index('@')], 0)
+        # write texts to Line 1 of the LCD.
+        lcd.write_line(word[word.index('@')+1:], 1)
+        sleep(time)
+        # Do GPIO cleanup manually before exiting.
         lcd.clear_lcd()
-        self._back_light(False)
+        GPIO.cleanup()
+    elif(len(word) > 16):
+        # Disable GPIO warnings
+        GPIO.setwarnings(False)
+        # Set GPIO pin mode. RPi pins described in this example use BCM.
+        GPIO.setmode(GPIO.BCM)
+
+        # create an instance of LCD1602GPIO_4BIT for 4-bit mode.
+        # the LCD module must be already powered on here.
+        # the instance initializes the LCD module immediately during init.
+        lcd = lcd1602gpio.LCD1602GPIO_4BIT(
+                rs=8,
+                e=7,
+                db7=18,
+                db6=23,
+                db5=24,
+                db4=25)
+
+        # write texts to Line 0 of the LCD.
+        lcd.write_line(word[:16], 0)
+        # write texts to Line 1 of the LCD.
+        lcd.write_line(word[16:], 1)
+        sleep(time)
+        # Do GPIO cleanup manually before exiting.
+        lcd.clear_lcd()
+        GPIO.cleanup()
+    else:
+        # Disable GPIO warnings
+        GPIO.setwarnings(False)
+        # Set GPIO pin mode. RPi pins described in this example use BCM.
+        GPIO.setmode(GPIO.BCM)
+
+        # create an instance of LCD1602GPIO_4BIT for 4-bit mode.
+        # the LCD module must be already powered on here.
+        # the instance initializes the LCD module immediately during init.
+        lcd = lcd1602gpio.LCD1602GPIO_4BIT(
+                rs=8,
+                e=7,
+                db7=18,
+                db6=23,
+                db5=24,
+                db4=25)
+
+        # write texts to Line 0 of the LCD.
+        lcd.write_line(word, 0)
+        sleep(time)
+        # Do GPIO cleanup manually before exiting.
+        lcd.clear_lcd()
         GPIO.cleanup()
 
-    def start_display(self, word: str, row: int = 0, time: int = 2):
-        """Inicia a tarefa de exibição no LCD em uma thread separada."""
-        if self._thread is None or not self._thread.is_alive():
-            self.word = word
-            self.row = row
-            self.time = time
-            self._running.set()
-            self._thread = threading.Thread(target=self._display_task)
-            self._thread.start()
-
-    def stop_display(self):
-        """Para a tarefa de exibição no LCD."""
-        if self._thread is not None and self._thread.is_alive():
-            self._running.clear()
-            self._thread.join()
-
-# Exemplo de uso:
-if __name__ == "__main__":
-    lcd_task = LCDTask()
-    lcd_task.start_display(word="SCA - UFC@Marlon Duarte")
-    sleep(5)  # Deixa a tarefa rodar por 5 segundos
-    lcd_task.stop_display()
+display_lcd(word="SCA@Marlon Duarte")
